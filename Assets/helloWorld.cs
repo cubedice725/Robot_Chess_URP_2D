3981978ad469e284c511a1ffaac1a0234850a0e2
cubedice <cubedice725@gmail.com>
2024-09-23 20:06:42 +0900

    2024.09.23 - 진행상황 - 리소스 문제로 2D로 바꿀예정
    - 특이사항 -
    게임을 완성하는것을 목표로함

---
+++ Assets/helloWorld.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class helloWorld : MonoBehaviour
+{
+    void Start()
+    {
+        print("helloWorld");
+    }
+}
